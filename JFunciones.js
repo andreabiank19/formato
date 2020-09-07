440849b681a1bff70c343903a6483f72f7652823
andreabiank19 <50815517+andreabiank19@users.noreply.github.com>
2020-09-07 15:41:10 -0500

    Update JFunciones.js

--- JFunciones.js
+++ JFunciones.js
@@ -1584,10 +1584,10 @@ function Calcular_Deuda_Financiera_CP() {
     for (var idx = 0; idx < PC_filas; idx++) {
         PC_S2 = PC_S2 + convNro(document.getElementById("PC_Monto_" + idx).value);
     }
-console.log("reactiva:"+Number(document.getElementById("deuda_reactiva").value))
+console.log("reactiva1:"+Number(document.getElementById("deuda_reactiva").value))
     var pasCP = convNro(S1) + convNro(PC_S2) + convNro(suma_cp);
     document.getElementById("bg_16").value = pasCP +Number(document.getElementById("deuda_reactiva").value) ;
-    document.getElementById("bg_16").innerHTML = Number(pasCP).toLocaleString('en');
+    document.getElementById("bg_16").innerHTML = Number(document.getElementById("bg_16").value).toLocaleString('en');
     return convNro(pasCP);
 }
 function Calcular_Pasivos_No_Corrientes() {
