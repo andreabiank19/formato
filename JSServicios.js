a629f05405455abdff8faed2e98cf09110ff5a99
andreabiank19 <50815517+andreabiank19@users.noreply.github.com>
2020-12-18 09:15:58 -0500

    Update JSServicios.js

--- JSServicios.js
+++ JSServicios.js
@@ -182,6 +182,7 @@ function Calcular_Ingreso_Manufactura() {
     Calcular_Informalidad();
     Calcular_Margen_Utilidad_Bruta();
     Calcular_EGP();
+    Calcular_EEFF();
 }
 function Calcular_Costo_Venta() {
     var costo1 = convNro(document.getElementById("costo_venta_op").value);
